1ea27e7c0b9e540e1252fd90280e152dfb1cb4df
Jaza <jazepstein@gmail.com>
2010-03-07 10:34:02 +1100

    use direct_to_template for static pages; fix conflict markers.

--- project/urls.py
+++ project/urls.py
@@ -1,16 +1,16 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
-from library.views import home, faq, about
 from contact.views import contact
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', home, name='home'),
-    url(r'^faq/$', faq, name='faq'),
-    url(r'^about/$', about, name='about'),
+    url(r'^$', direct_to_template, {'template': 'home.html'}, name='home'),
+    url(r'^faq/$', direct_to_template, {'template': 'faq.html'}, name='faq'),
+    url(r'^about/$', direct_to_template, {'template': 'about.html'}, name='about'),
     url(r'^contact/$', contact, name='contact'),
     (r'^accounts/', include('accounts.urls')),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
