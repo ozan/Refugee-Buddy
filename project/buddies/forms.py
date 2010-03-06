a86dd62b41c02cbe50d9db61637f55f098e9bedd
Ozan Onay <ozan.onay@gmail.com>
2010-03-06 14:44:46 +1100

    Made the geo distance comparison actually work

--- project/buddies/forms.py
+++ project/buddies/forms.py
@@ -2,6 +2,16 @@ from django import forms
 
 from models import Buddy
 
+RADIUS_CHOICES = (
+    (1, '1km'),
+    (10, '10km'),
+    (100, '100km')
+)
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Buddy
+        
+class SearchForm(forms.Form):
+    location = forms.CharField(max_length=100, required=True)
+    radius = forms.ChoiceField(choices=RADIUS_CHOICES)
--- project/buddies/views.py
+++ project/buddies/views.py
@@ -1,11 +1,28 @@
+from urllib import urlencode
+from urllib2 import urlopen
+import simplejson
+
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 
-from forms import ProfileForm
+from forms import ProfileForm, SearchForm
 from models import Buddy
+
+from library.geo import points2distance
+
+GEOCODER_URL = 'http://maps.google.com/maps/geo?q=%s&output=json&sensor=false&key=' + settings.GOOGLE_MAPS_API_KEY    
+
+def geocode(location_str):
+    result = urlopen(GEOCODER_URL % urlencode({'q': location_str}))
+    data = simplejson.loads(result.read())
+    if not data['Status']['code'] == 200:
+        pass #handle error
+    lng, lat, _ = data['Placemark'][0]['Point']['coordinates']
+    return float(lat), float(lng)
 
 @login_required
 def search(request):
@@ -19,21 +36,27 @@ def search(request):
     except AttributeError:
         return HttpResponseForbidden()
     
-    buddies_in_range = []
-    if request.method == 'POST':
+    form = SearchForm(request.POST or None)
+    if form.is_valid():
+        buddies_in_range = []
         # geocode address
-        lat, lng = magical_geocode_function(request.method.get('location'))
+        lat, lng = geocode(request.POST.get('location'))
         # to keep things super simple, pull out all buddies, then filter in
         # memory. obviously this will have to be fixed once there is a
         # non-trivial number of buddies in the db. and when we have more 
         # than a day to work on this thing
         buddies = Buddy.objects.all()
         for buddy in buddies:
-            buddy_lat, buddy_lng = buddy.location.latitude, buddy.location.longitude
-            if (buddy_lat - lat) ** 2 + (buddy_lng - lng) ** 2 <= request.POST.get('range') ** 2:
+            buddy_lat, buddy_lng = float(buddy.location['latitude']), float(buddy.location['longitude'])
+            radius = int(request.POST.get('radius'))
+            dist = points2distance(((buddy_lng, 0, 0), (buddy_lat, 0, 0)),((lng, 0, 0), (lat, 0, 0)))
+            if dist <= radius:
                 buddies_in_range.append(buddy)
-
+    else:
+        buddies_in_range = None
+        
     return render_to_response('buddies/search.html', {
+        'form': form,
         'buddies_in_range': buddies_in_range
     }, context_instance=RequestContext(request))
 
