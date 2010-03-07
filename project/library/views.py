bb9e4bb729eb645955ce2214e5f05c9b217757b1
Ozan Onay <ozan.onay@gmail.com>
2010-03-07 11:25:46 +1100

    Fixed template paths for static views

--- project/library/views.py
+++ project/library/views.py
@@ -4,11 +4,11 @@ from django.template import RequestContext
 def home(request):
     if request.user.is_authenticated():
         return redirect('buddies_my_detail')
-    return render_to_response('home.html', context_instance=RequestContext(request))
+    return render_to_response('static/home.html', context_instance=RequestContext(request))
 
 def faq(request):
-    return render_to_response('faq.html', context_instance=RequestContext(request))
+    return render_to_response('static/faq.html', context_instance=RequestContext(request))
 
 def about(request):
-    return render_to_response('about.html', context_instance=RequestContext(request))
+    return render_to_response('static/about.html', context_instance=RequestContext(request))
 
